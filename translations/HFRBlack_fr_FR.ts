56acee79fef4cb1b536e10e5c155ecbbc1aebd87
Pierre Lebreton <pierre@mbp.local>
2014-04-06 13:27:07 +0200

    + add French translations

---
+++ translations/HFRBlack_fr_FR.ts
@@ -0,0 +1,441 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>DisplayCategoryListPage</name>
+    <message>
+        <location filename="../assets/DisplayCategoryListPage.qml" line="13"/>
+        <source>HFR 4 BB</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExploreCategory</name>
+    <message>
+        <location filename="../assets/ExploreCategory.qml" line="193"/>
+        <source>Navigation</source>
+        <translation>Navigation</translation>
+    </message>
+    <message>
+        <location filename="../assets/ExploreCategory.qml" line="196"/>
+        <location filename="../assets/ExploreCategory.qml" line="272"/>
+        <source>First page</source>
+        <translation>Première page</translation>
+    </message>
+    <message>
+        <location filename="../assets/ExploreCategory.qml" line="203"/>
+        <source>Last page</source>
+        <translation>Dernière page</translation>
+    </message>
+    <message>
+        <location filename="../assets/ExploreCategory.qml" line="210"/>
+        <source>Last page read</source>
+        <translation>Dernière page lue</translation>
+    </message>
+    <message>
+        <location filename="../assets/ExploreCategory.qml" line="280"/>
+        <source>Prev page</source>
+        <translation>Page précédente</translation>
+    </message>
+    <message>
+        <location filename="../assets/ExploreCategory.qml" line="288"/>
+        <source>Next page</source>
+        <translation>Page suivante</translation>
+    </message>
+</context>
+<context>
+    <name>ExploreCategoryController</name>
+    <message>
+        <location filename="../src/ExploreCategoryController.cpp" line="72"/>
+        <source>Error: %1 status: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Favorite</name>
+    <message>
+        <location filename="../assets/Favorite.qml" line="102"/>
+        <source>Navigation</source>
+        <translation>Navigation</translation>
+    </message>
+    <message>
+        <location filename="../assets/Favorite.qml" line="105"/>
+        <source>First page</source>
+        <translation>Première page</translation>
+    </message>
+    <message>
+        <location filename="../assets/Favorite.qml" line="112"/>
+        <source>Last page</source>
+        <translation>Dernière page</translation>
+    </message>
+    <message>
+        <location filename="../assets/Favorite.qml" line="119"/>
+        <source>Last page read</source>
+        <translation>Dernière page lue</translation>
+    </message>
+</context>
+<context>
+    <name>ListFavoriteController</name>
+    <message>
+        <location filename="../src/ListFavoriteController.cpp" line="58"/>
+        <source>Error: %1 status: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoginController</name>
+    <message>
+        <location filename="../src/LoginController.cpp" line="67"/>
+        <source>Error: %1 status: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LoginController.cpp" line="75"/>
+        <source>Login failed</source>
+        <translation>Erreur lors de l&apos;authentification</translation>
+    </message>
+</context>
+<context>
+    <name>LoginForm</name>
+    <message>
+        <location filename="../assets/LoginForm.qml" line="7"/>
+        <location filename="../assets/LoginForm.qml" line="30"/>
+        <source>Login</source>
+        <translation>Utilisateur</translation>
+    </message>
+    <message>
+        <location filename="../assets/LoginForm.qml" line="10"/>
+        <source>Log</source>
+        <translation>Se connecter</translation>
+    </message>
+    <message>
+        <location filename="../assets/LoginForm.qml" line="18"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../assets/LoginForm.qml" line="41"/>
+        <source>Password</source>
+        <translation>Mot de passe</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindowTabbedPane</name>
+    <message>
+        <location filename="../assets/MainWindowTabbedPane.qml" line="11"/>
+        <source>Forum</source>
+        <translation>Forum</translation>
+    </message>
+    <message>
+        <location filename="../assets/MainWindowTabbedPane.qml" line="23"/>
+        <source>MP</source>
+        <translation>MP</translation>
+    </message>
+    <message>
+        <location filename="../assets/MainWindowTabbedPane.qml" line="35"/>
+        <source>Options</source>
+        <translation>Options</translation>
+    </message>
+</context>
+<context>
+    <name>PostMessage</name>
+    <message>
+        <location filename="../assets/PostMessage.qml" line="25"/>
+        <source>Reply</source>
+        <translation>Répondre</translation>
+    </message>
+    <message>
+        <location filename="../assets/PostMessage.qml" line="28"/>
+        <source>Send</source>
+        <translation>Envoyer</translation>
+    </message>
+    <message>
+        <location filename="../assets/PostMessage.qml" line="56"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../assets/PostMessage.qml" line="114"/>
+        <source>Quote</source>
+        <translation>Citer</translation>
+    </message>
+    <message>
+        <location filename="../assets/PostMessage.qml" line="121"/>
+        <source>Code</source>
+        <translation>Code</translation>
+    </message>
+    <message>
+        <location filename="../assets/PostMessage.qml" line="128"/>
+        <source>Bold</source>
+        <translation>Gras</translation>
+    </message>
+    <message>
+        <location filename="../assets/PostMessage.qml" line="134"/>
+        <source>Italic</source>
+        <translation>Italique</translation>
+    </message>
+    <message>
+        <location filename="../assets/PostMessage.qml" line="140"/>
+        <source>Underline</source>
+        <translation>Souligner</translation>
+    </message>
+</context>
+<context>
+    <name>PostMessageController</name>
+    <message>
+        <location filename="../src/PostMessageController.cpp" line="86"/>
+        <location filename="../src/PostMessageController.cpp" line="170"/>
+        <location filename="../src/PostMessageController.cpp" line="191"/>
+        <source>Error: %1 status: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/PostMessageController.cpp" line="94"/>
+        <source>Post failed</source>
+        <translation>Erreur lors de la publication du post</translation>
+    </message>
+</context>
+<context>
+    <name>PrivateMessage</name>
+    <message>
+        <location filename="../assets/PrivateMessage.qml" line="74"/>
+        <source>[Not Read] </source>
+        <translation>[Non lu] </translation>
+    </message>
+    <message>
+        <location filename="../assets/PrivateMessage.qml" line="113"/>
+        <source>Navigation</source>
+        <translation>Navigation</translation>
+    </message>
+    <message>
+        <location filename="../assets/PrivateMessage.qml" line="116"/>
+        <source>First page</source>
+        <translation>Première page</translation>
+    </message>
+    <message>
+        <location filename="../assets/PrivateMessage.qml" line="123"/>
+        <source>Last page read</source>
+        <translation>Dernière page lue</translation>
+    </message>
+</context>
+<context>
+    <name>PrivateMessageController</name>
+    <message>
+        <location filename="../src/PrivateMessageController.cpp" line="59"/>
+        <source>Error: %1 status: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RefreshHeader</name>
+    <message>
+        <location filename="../assets/RefreshHeader.qml" line="28"/>
+        <location filename="../assets/RefreshHeader.qml" line="71"/>
+        <source>Pull to refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/RefreshHeader.qml" line="66"/>
+        <source>Release to refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/RefreshHeader.qml" line="73"/>
+        <source>Pull to refresh. Last refreshed </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/RefreshHeader.qml" line="107"/>
+        <source>%L1y ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/RefreshHeader.qml" line="109"/>
+        <location filename="../assets/RefreshHeader.qml" line="115"/>
+        <source>%L1m ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/RefreshHeader.qml" line="111"/>
+        <source>%L1d ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/RefreshHeader.qml" line="113"/>
+        <source>%L1h ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/RefreshHeader.qml" line="116"/>
+        <source>just now</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../assets/Settings.qml" line="16"/>
+        <location filename="../assets/Settings.qml" line="44"/>
+        <location filename="../assets/Settings.qml" line="64"/>
+        <location filename="../assets/Settings.qml" line="74"/>
+        <source>User: </source>
+        <translation>Utilisateur: </translation>
+    </message>
+    <message>
+        <location filename="../assets/Settings.qml" line="23"/>
+        <source>Connect</source>
+        <translation>Se connecter</translation>
+    </message>
+    <message>
+        <location filename="../assets/Settings.qml" line="38"/>
+        <source>log out</source>
+        <translation>Se deconnecter</translation>
+    </message>
+</context>
+<context>
+    <name>ShowThreadController</name>
+    <message>
+        <location filename="../src/ShowThreadController.cpp" line="70"/>
+        <location filename="../src/ShowThreadController.cpp" line="303"/>
+        <source>Error: %1 status: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SmileyPicker</name>
+    <message>
+        <location filename="../assets/SmileyPicker.qml" line="33"/>
+        <source>smiley</source>
+        <translation>Smiley</translation>
+    </message>
+    <message>
+        <location filename="../assets/SmileyPicker.qml" line="40"/>
+        <source>Default</source>
+        <translation>Défaut</translation>
+    </message>
+    <message>
+        <location filename="../assets/SmileyPicker.qml" line="48"/>
+        <source>Search</source>
+        <translation>Chercher</translation>
+    </message>
+    <message>
+        <location filename="../assets/SmileyPicker.qml" line="56"/>
+        <source>Next page</source>
+        <translation>Page suivante</translation>
+    </message>
+    <message>
+        <location filename="../assets/SmileyPicker.qml" line="64"/>
+        <source>Previous page</source>
+        <translation>Page précédente</translation>
+    </message>
+</context>
+<context>
+    <name>SmileyPickerController</name>
+    <message>
+        <location filename="../src/SmileyPickerController.cpp" line="105"/>
+        <source>Error: %1 status: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ThreadPage</name>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="156"/>
+        <source>Edit</source>
+        <translation>Editer</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="177"/>
+        <source>Quote more</source>
+        <translation>Citer (plus)</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="170"/>
+        <source>Quote</source>
+        <translation>Citer</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="153"/>
+        <source>Actions</source>
+        <translation>Actions</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="163"/>
+        <source>Add Favorite</source>
+        <translation>Ajouter aux favoris</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="252"/>
+        <source>Friendly warning</source>
+        <translation>Attention</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="253"/>
+        <source>You are going to leave the application, do you want to continue?</source>
+        <translation>Vous êtes sur le pont de quitter l&apos;application, êtes vous sûr de vouloir continuer?</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="263"/>
+        <source>Reply</source>
+        <translation>Répondre</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="283"/>
+        <source>First page</source>
+        <translation>Première page</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="291"/>
+        <source>Last page</source>
+        <translation>Dernière page</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="299"/>
+        <source>Prev page</source>
+        <translation>Page précédente</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="307"/>
+        <source>Next page</source>
+        <translation>Page suivante</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="316"/>
+        <source>To Top</source>
+        <translation>Haut de page</translation>
+    </message>
+    <message>
+        <location filename="../assets/ThreadPage.qml" line="323"/>
+        <source>To Bottom</source>
+        <translation>Bas de page</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="27"/>
+        <source>Home</source>
+        <translation>Catégories</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="43"/>
+        <source>Favorite</source>
+        <translation>Favoris</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="57"/>
+        <source>Message</source>
+        <translation>Message</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="70"/>
+        <source>Search</source>
+        <translation>Rechercher</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="86"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+</context>
+</TS>
