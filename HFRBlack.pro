95cee97c5d8f9e5893c92fa21ad1c2e5fc670dfd
Amonchakai <pierre@mbp.local>
2014-05-17 14:49:19 +0200

    + cleanup project

--- HFRBlack.pro
+++
@@ -1,7 +0,0 @@
-APP_NAME = HFR10
-
-CONFIG += qt warn_on cascades10 
-QT += network xml
-LIBS += -lbbsystem
-
-include(config.pri)
--- src/Globals.cpp
+++
@@ -1,7 +0,0 @@
-#include "Globals.h"
-
-QString DefineConsts::HARDWARE_FR_URL = "http://www.hardware.fr";
-QString DefineConsts::FORUM_URL		  = "http://forum.hardware.fr";
-QString DefineConsts::REHOST_URL	  = "http://reho.st";
-
-
